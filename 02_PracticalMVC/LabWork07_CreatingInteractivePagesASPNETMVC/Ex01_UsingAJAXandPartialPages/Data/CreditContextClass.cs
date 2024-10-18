e6c92c51218e6a8fbadf24ae5771c2cbae90b922
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-10-18 12:00:08 +0300

    completed laboratory work No. 7
    completed laboratory work No. 7 "Creating interactive pages in ASP.NET MVC":
    - exercise 1. Using AJAX and partial pages

---
+++ 02_PracticalMVC/LabWork07_CreatingInteractivePagesASPNETMVC/Ex01_UsingAJAXandPartialPages/Data/CreditContextClass.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ex01_UsingAJAXandPartialPages.Models;
+using Microsoft.CodeAnalysis.Differencing;
+using System.Security.Cryptography;
+
+namespace Ex01_UsingAJAXandPartialPages.Data
+{
+        public class CreditContextClass : DbContext
+        {
+            public CreditContextClass(DbContextOptions<CreditContextClass> options)
+            : base(options)
+            { }
+        public DbSet<CreditClass> Credits { get; set; }
+        public DbSet<BidClass> Bids { get; set; }
+    }
+}
---
+++ 02_PracticalMVC/LabWork07_CreatingInteractivePagesASPNETMVC/Ex01_UsingAJAXandPartialPages/Models/SeedData.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ex01_UsingAJAXandPartialPages.Data;
+
+namespace Ex01_UsingAJAXandPartialPages.Models
+{
+    public class SeedData
+    {   
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new CreditContextClass(
+            serviceProvider.GetRequiredService<
+            DbContextOptions<CreditContextClass>>()))
+            {   
+                if( context == null || context.Credits == null )
+                {
+                    throw new ArgumentNullException("Null CreditContext");
+                }
+                // Если в базе данных уже есть кредиты,
+                // то возвращается инициализатор заполнения и кредиты не добавляются
+                if( context.Credits.Any() )
+                {
+                    return;
+                }
+                context.Credits.Add(new CreditClass { Head = "Ипотечный", Period = 10, Sum = 1000000, Procent = 15 });
+
+                context.Credits.Add(new CreditClass { Head = "Образовательный", Period = 7, Sum = 300000, Procent = 10 });
+                context.Credits.Add(new CreditClass { Head = "Потребительский", Period = 5, Sum = 500000, Procent = 19 });
+                // можно использовать метод AddRange
+                context.Credits.AddRange(
+                new CreditClass
+                {
+                    Head = "Льготный",
+                    Period = 12,
+                    Sum = 55555,
+                    Procent = 7
+                },
+                new CreditClass
+                {
+                    Head = "Срочный",
+                    Period = 3,
+                    Sum = 3333,
+                    Procent = 19
+                }
+                );
+                context.SaveChanges();
+            }   
+        }
+    }   
+}
